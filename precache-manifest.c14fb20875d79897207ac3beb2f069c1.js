190497601309fc316b2059d576e3c5f700085779
Mohamed Nasser <aheart500@yahoo.com>
2019-10-08 04:07:46 +0200

    Updates

---
+++ precache-manifest.c14fb20875d79897207ac3beb2f069c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bce95ea875f2f7bb99f731d74c20b8d",
+    "url": "/NTF/index.html"
+  },
+  {
+    "revision": "c6ee03f015084d8ec1ff",
+    "url": "/NTF/static/css/main.77b7af96.chunk.css"
+  },
+  {
+    "revision": "56bd888aebe8af44f175",
+    "url": "/NTF/static/js/2.8547fac0.chunk.js"
+  },
+  {
+    "revision": "c6ee03f015084d8ec1ff",
+    "url": "/NTF/static/js/main.6cd7541f.chunk.js"
+  },
+  {
+    "revision": "aa6cacc67e36ce8d4dcd",
+    "url": "/NTF/static/js/runtime-main.914e0eff.js"
+  },
+  {
+    "revision": "5989599aefecb81702ed22924b04960f",
+    "url": "/NTF/static/media/banner-back.5989599a.jpg"
+  },
+  {
+    "revision": "3f24a8ae699bf8028c0956888887884a",
+    "url": "/NTF/static/media/engineering-small-back.3f24a8ae.jpg"
+  },
+  {
+    "revision": "268887aaa8eaecfdf5271782602b74ac",
+    "url": "/NTF/static/media/medicine-small-back.268887aa.jpg"
+  },
+  {
+    "revision": "e2f5a447f8df032b991c81d69eeacbb2",
+    "url": "/NTF/static/media/sciences-small-back.e2f5a447.jpg"
+  },
+  {
+    "revision": "dea9fc059932791883e10ba571fb31ba",
+    "url": "/NTF/static/media/veterinary-small-back.dea9fc05.png"
+  }
+]);
